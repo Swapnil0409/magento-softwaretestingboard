14c11470d2b9bd5ad156825732350f9002bd6316
swapnilbagal <swapnilbagal18@gmail.com>
2024-02-06 18:16:54 +0530

    Added Test case for women dropdown

---
+++ src/test/java/test_cases/women/VerifyFunctionalityOfNavigatingToWomenTopsJacketsPage.java
@@ -0,0 +1,13 @@
+package test_cases.women;
+
+import base_tests.BaseBrowserTest;
+import org.testng.annotations.Test;
+
+public class VerifyFunctionalityOfNavigatingToWomenTopsJacketsPage extends BaseBrowserTest {
+
+    @Test
+    public void verifyNavigationFunctionalityToWomenTopsJackets() {
+
+    }
+
+}
